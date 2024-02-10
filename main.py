5ed496308a882debb12ca5d69a1f67183b5e7c20
Sumit Suryawanshi <suryawanshisumit2003@gmail.com>
2024-02-10 18:54:45 +0530

    second commit_9

--- main.py
+++ main.py
@@ -80,6 +80,6 @@ async def extract_text(img: UploadFile = File(...)):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="127.0.0.1", port=8081)
+    uvicorn.run(app, host="0.0.0.0", port=10000)
 
 
